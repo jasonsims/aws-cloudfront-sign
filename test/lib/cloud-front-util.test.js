2eb9fe126b119718cfc99bd887fd67f9870bc77c
Jason Sims <jsims@nutanix.com>
2014-11-25 22:05:13 -0800

    Update library to conform to the node style guide
    This commit updates the libary to conform to the node style guide
    defined by felixge/node-style-guide.
    
    https://github.com/felixge/node-style-guide

--- test/lib/cloud-front-util.test.js
+++ test/lib/cloud-front-util.test.js
@@ -1,24 +1,29 @@
-var path = require('path')
-  , url = require('url')
-  , querystring = require('querystring')
-  , fs = require('fs')
-  , test = require("tap").test
-  , cf = require(path.join(process.cwd(), 'lib', 'cloudfront-util'))
-  , DEFAULT_PARAMS = {
-      keypairId: 'ABC123'
-    , privateKeyString: fs.readFileSync(path.join(process.cwd(), 'test', 'files', 'dummy.pem')).toString('ascii')
-  }
-;
+var path = require('path');
+var url = require('url');
+var querystring = require('querystring');
+var fs = require('fs');
+var test = require('tap').test;
+var cf = require(path.join(process.cwd(), 'lib', 'cloudfront-util'));
+var DEFAULT_PARAMS = {
+  keypairId: 'ABC123',
+  privateKeyString: fs.readFileSync(
+    path.join(process.cwd(), 'test/files/dummy.pem')).toString('ascii')
+};
 
 test('canned policy', function(t) {
-  var now = new Date()
-    , result = url.parse(cf.getSignedUrl('http://foo.com', DEFAULT_PARAMS));
+  var now = new Date();
+  var result = url.parse(cf.getSignedUrl('http://foo.com', DEFAULT_PARAMS));
+  var expireDiff;
 
   result.query = querystring.parse(result.search.split('?')[1]);
   t.equal(result.hostname, 'foo.com', 'it should set the appropriate domain');
-  t.equal(result.query['Key-Pair-Id'], DEFAULT_PARAMS.keypairId, 'it should set the key pair id');
-  var expireDiff = Math.abs(new Date(result.query.Expires * 1000).getSeconds() - now.getSeconds());
-  t.ok(29 <= expireDiff && 31 >= expireDiff, 'it should default `expires` to ~30 seconds from creation date')
+  t.equal(result.query['Key-Pair-Id'], DEFAULT_PARAMS.keypairId,
+    'it should set the key pair id');
+
+  expireDiff = Math.abs(
+    new Date(result.query.Expires * 1000).getSeconds() - now.getSeconds());
+  t.ok(29 <= expireDiff && 31 >= expireDiff,
+    'it should default `expires` to ~30 seconds from creation date');
   t.end();
 });
 
@@ -26,16 +31,16 @@ test('signature', function(t) {
   var result = parseUrl(cf.getSignedUrl('http://foo.com', DEFAULT_PARAMS));
   t.ok(result.query.hasOwnProperty('Signature'), 'it should be created');
   t.end();
-})
+});
 
 test('signature#_normalizeSignature', function(t) {
-  var illegalChars = ['+', '=', '/']
-  var arg = illegalChars.join('')
-  var sig = cf._normalizeSignature(arg)
+  var illegalChars = ['+', '=', '/'];
+  var arg = illegalChars.join('');
+  var sig = cf._normalizeSignature(arg);
 
   t.ok('-_~', 'it should translate illegal characters');
   t.end();
-})
+});
 
 /**
  * Parse a URL string
@@ -44,8 +49,8 @@ test('signature#_normalizeSignature', function(t) {
  * @return {Object}
  */
 function parseUrl(str) {
-  var _url = url.parse(str)
-  _url.query = querystring.parse(_url.query)
+  var _url = url.parse(str);
+  _url.query = querystring.parse(_url.query);
 
-  return _url
+  return _url;
 }
