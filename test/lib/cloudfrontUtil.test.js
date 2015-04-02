6ef312ed2fc745dd59f2e6b87c8ff15bd810f7d7
Jason Sims <jsims@nutanix.com>
2015-04-01 17:56:06 -0700

    Refactor test suite
    This commit fixes up the test suite failures that were ocurring
    during build. It also migrates the test framework from tap to
    mocha and adds additional coverage.

--- test/lib/cloudfrontUtil.test.js
+++ test/lib/cloudfrontUtil.test.js
@@ -1,134 +1,229 @@
-var _ = require('lodash')
+/**
+ * Cloudfront utility tests
+ *
+ * TODO: Add mock for crypto methods
+ * TODO: Add mock for fs methods
+ * TODO: Add mock for CannedPolicy methods
+ */
+
+var fs = require('fs');
 var path = require('path');
 var url = require('url');
-var moment = require('moment')
-var querystring = require('querystring');
-var fs = require('fs');
-var test = require('tap').test;
-var cf = require(path.join(process.cwd(), 'lib', 'cloudfrontUtil'));
-var DEFAULT_PARAMS = {
-  keypairId: 'ABC123',
-  privateKeyString: fs.readFileSync(
-    path.join(process.cwd(), 'test/files/dummy.pem')).toString('ascii')
-};
+var _ = require('lodash');
+var chai = require('chai');
+var expect = chai.expect;
+var moment = require('moment');
+var sinon = require('sinon');
+var CloudfrontUtil = require('../../lib/cloudfrontUtil');
 
-test('canned policy', function (t) {
-  var now = new Date().getTime() / 1000;
-  var testUrl = cf.getSignedUrl('http://foo.com', DEFAULT_PARAMS)
-  var result = url.parse(testUrl, true);
-  var expireDiff = (parseInt(result.query.Expires) - now)
+describe('CloudfrontUtil', function() {
+  var defaultParams;
+  var pkey;
+  var pkeyPath;
+  var clock;
 
-  t.equal(result.hostname, 'foo.com', 'it should set the appropriate domain');
-  t.equal(result.query['Key-Pair-Id'], DEFAULT_PARAMS.keypairId,
-    'it should set the key pair id');
-  t.ok(29 <= expireDiff && 31 >= expireDiff,
-    'it should default `expires` to 30 seconds from creation date');
-  t.end();
-});
-
-test('signature', function (t) {
-  var result = parseUrl(cf.getSignedUrl('http://foo.com', DEFAULT_PARAMS));
-  t.ok(result.query.hasOwnProperty('Signature'), 'it should be created');
-  t.end();
-});
-
-test('cloudfrontUtil#getSignedUrl()', function(t) {
-  var testExpireTime = moment().add(1, 'day');
-  var params = _.extend({}, DEFAULT_PARAMS, {expireTime: testExpireTime});
-  var res = cf.getSignedUrl('http://foo.com', params);
-  var resUrl = url.parse(res, true);
-  var actualTime = parseInt(resUrl.query.Expires);
-  var expectedTime = testExpireTime.unix();
-
-  t.ok(expectedTime === actualTime, 'should accept a moment object');
-  t.end();
-})
-
-test('signature#_normalizeSignature', function (t) {
-  var illegalChars = ['+', '=', '/'];
-  var arg = illegalChars.join('');
-  var sig = cf._normalizeSignature(arg);
-
-  t.ok('-_~', 'it should translate illegal characters');
-  t.end();
-});
-
-test('canned policy types', function (t) {
-  var result1 = url.parse(cf.getSignedUrl('http://foo.com', {
-    keypairId: 'ABC123',
-    privateKeyString: fs.readFileSync(
-      path.join(process.cwd(), 'test/files/dummy.pem')).toString('ascii'),
-    expireTime: ((new Date(2033, 1, 1)).getTime()).toString()
-  }));
-  t.ok(result1.path.indexOf('=1990857600') > -1, 'it should support legacy unix time strings as expire times.');
-
-  var result2 = url.parse(cf.getSignedUrl('http://foo.com', {
-    keypairId: 'ABC123',
-    privateKeyString: fs.readFileSync(
-      path.join(process.cwd(), 'test/files/dummy.pem')).toString('ascii'),
-    expireTime: ((new Date(2033, 1, 1)).getTime())
-  }));
-  t.ok(result2.path.indexOf('=1990857600') > -1, 'it should support integer time expire times.');
-
-  var result3 = url.parse(cf.getSignedUrl('http://foo.com', {
-    keypairId: 'ABC123',
-    privateKeyString: fs.readFileSync(
-      path.join(process.cwd(), 'test/files/dummy.pem')).toString('ascii'),
-    expireTime: new Date(2033, 1, 1)
-  }));
-  t.ok(result3.path.indexOf('=1990857600') > -1, 'it should support Date objects as expire times.');
-
-  var result4;
-
-  try {
-    result4 = url.parse(cf.getSignedUrl('http://foo.com', {
+  // Setup
+  //------
+  beforeEach(function(done) {
+    clock = sinon.useFakeTimers();
+    pkeyPath = './test/files/dummy.pem';
+    pkey = fs.readFileSync(path.join(process.cwd(), pkeyPath));
+    defaultParams = {
       keypairId: 'ABC123',
-      privateKeyString: fs.readFileSync(
-        path.join(process.cwd(), 'test/files/dummy.pem')).toString('ascii'),
-      expireTime: new Date(1000, 1, 1)
-    }));
-  } catch (e) {
-  }
-  t.ok(typeof(result4) == 'undefined', 'it should alert the developer about urls that have already expired.');
+      privateKeyString: pkey.toString('ascii')
+    };
 
-  var result5;
-
-  try {
-    result5 = url.parse(cf.getSignedUrl('http://foo.com', {
-      keypairId: 'ABC123',
-      privateKeyString: fs.readFileSync(
-        path.join(process.cwd(), 'test/files/dummy.pem')).toString('ascii'),
-      expireTime: new Date(3000, 1, 1)
-    }));
-  } catch (e) {
-  }
-  t.ok(typeof(result5) == 'undefined', 'it should alert the developer about invalid unix dates that AWS will not accept.');
-
-  t.end();
-});
-
-test('RTMP URL Objects', function (t) {
-  var result1 = cf.getSignedRTMPUrl('xxxxxx.cloudfront.net', 'mykeyfolder/mykey.mp4', {
-    keypairId: 'ABC123',
-    privateKeyString: fs.readFileSync(
-      path.join(process.cwd(), 'test/files/dummy.pem')).toString('ascii'),
-    expireTime: new Date(2033, 1, 1)
+    done();
   });
-  t.ok(result1.rtmpServerPath.indexOf('cfx/st') > -1, 'it should return a properly formatted rtmp server path.');
-  t.ok(result1.rtmpStreamName.indexOf('Expires=1990857600') > -1, 'it should return a properly formatted rtmp stream name.');
 
-  t.end();
+  afterEach(function(done) {
+    clock.restore();
+    done();
+  });
+
+  // Tests
+  //------
+  describe('#getSignedUrl()', function() {
+    it('should accept `expireTime` as a string', function(done) {
+      var testExpireTime = moment().add(1, 'day').unix() * 1000;
+      var params = _.extend({}, defaultParams, {
+        expireTime: testExpireTime.toString()
+      });
+      var result = CloudfrontUtil.getSignedUrl('http://foo.com', params);
+      var parsedResult = url.parse(result, true);
+      var expectedResult = (testExpireTime / 1000).toString();
+
+      expect(parsedResult.query.Expires).to.equal(expectedResult);
+      done();
+    });
+
+    it('should accept `expireTime` as a number', function(done) {
+      var testExpireTime = moment().add(1, 'day').unix() * 1000;
+      var params = _.extend({}, defaultParams, {
+        expireTime: testExpireTime
+      });
+      var result = CloudfrontUtil.getSignedUrl('http://foo.com', params);
+      var parsedResult = url.parse(result, true);
+      var expectedResult = (testExpireTime / 1000).toString();
+
+      expect(parsedResult.query.Expires).to.equal(expectedResult);
+      done();
+    });
+
+    it('should accept `expireTime` as a moment', function(done) {
+      var testExpireTime = moment().add(1, 'day');
+      var params = _.extend({}, defaultParams, {
+        expireTime: testExpireTime
+      });
+      var result = CloudfrontUtil.getSignedUrl('http://foo.com', params);
+      var parsedResult = url.parse(result, true);
+      var expectedResult = testExpireTime.unix().toString();
+
+      expect(parsedResult.query.Expires).to.equal(expectedResult);
+      done();
+    });
+
+    it('should accept `expireTime` as a Date', function(done) {
+      var testExpireTime = new Date(new Date().getTime() + 10000);
+      var params = _.extend({}, defaultParams, {
+        expireTime: testExpireTime
+      });
+      var result = CloudfrontUtil.getSignedUrl('http://foo.com', params);
+      var parsedResult = url.parse(result, true);
+      var expectedResult =
+        Math.round(testExpireTime.getTime() / 1000).toString();
+
+      expect(parsedResult.query.Expires).to.equal(expectedResult);
+      done();
+    });
+
+    it('should accept private key as string', function(done) {
+      var testExpireTime = moment().add(1, 'day').unix() * 1000;
+      var params = _.extend({}, defaultParams);
+      var result = CloudfrontUtil.getSignedUrl('http://foo.com', params);
+      var parsedResult = url.parse(result, true);
+
+      expect(parsedResult.query).to.have.property('Signature');
+      done();
+    });
+
+    it('should accept private key as filepath', function(done) {
+      var testExpireTime = moment().add(1, 'day').unix() * 1000;
+      var params = _.extend({}, defaultParams, {
+        privateKeyPath: path.join(process.cwd(), pkeyPath)
+      });
+      var result;
+      var parsedResult;
+
+      delete params.privateKeyString;
+      result = CloudfrontUtil.getSignedUrl('http://foo.com', params);
+      parsedResult = url.parse(result, true);
+
+      expect(parsedResult.query).to.have.property('Signature');
+      done();
+    });
+
+    it('should default `expireTime` to 30 seconds', function(done) {
+      var params = _.extend({}, defaultParams);
+      var result = CloudfrontUtil.getSignedUrl('http://foo.com', params);
+      var parsedResult = url.parse(result, true);
+
+      expect(parsedResult.query.Expires).to.equal('30');
+      done();
+    });
+
+    it('should include original query params', function(done) {
+      var params = _.extend({}, defaultParams);
+      var result = CloudfrontUtil.getSignedUrl(
+        'http://foo.com?test=true', params);
+      var parsedResult = url.parse(result, true);
+
+      expect(parsedResult.query).to.have.property('test', 'true');
+      expect(parsedResult.query).to.have.property('Expires');
+      expect(parsedResult.query).to.have.property('Signature');
+      expect(parsedResult.query).to.have.property('Key-Pair-Id');
+
+      done();
+    });
+
+    it('should return a signed URL', function(done) {
+      var params = _.extend({}, defaultParams);
+      var result = CloudfrontUtil.getSignedUrl('http://foo.com', params);
+      var parsedResult = url.parse(result, true);
+
+      expect(parsedResult.query).to.have.property('Expires');
+      expect(parsedResult.query).to.have.property('Signature');
+      expect(parsedResult.query).to.have.property('Key-Pair-Id');
+
+      done();
+    });
+  });
+
+  describe('#getSignedRTMPUrl()', function() {
+    var signedUrlStub;
+
+    beforeEach(function(done) {
+      signedUrlStub = sinon.stub(CloudfrontUtil, 'getSignedUrl', _.noop);
+      done();
+    });
+
+    afterEach(function(done) {
+      signedUrlStub.restore();
+      done();
+    });
+
+    it('should return RTMP object', function(done) {
+      var params = 'test';
+      var s3key = 'test/key';
+      var res = CloudfrontUtil.getSignedRTMPUrl('foo.com', s3key, params);
+
+      expect(signedUrlStub.calledWith(s3key, params)).to.equal(true);
+      expect(res).to.have.property('rtmpServerPath', 'rtmp://foo.com/cfx/st');
+      expect(res).to.have.property('rtmpStreamName');
+
+      done();
+    });
+
+    it('should fail if `domainname` is missing', function(done) {
+      var fn = function() {CloudfrontUtil.getSignedRTMPUrl();};
+      expect(fn).to.throw(Error, /domain name doesn't look right/i);
+      done();
+    });
+
+    it('should fail if `domainname` is invalid', function(done) {
+      var fn = function() {CloudfrontUtil.getSignedRTMPUrl('https://foo.com');};
+      expect(fn).to.throw(Error, /domain name doesn't look right/i);
+      done();
+    });
+
+    it('should fail if `s3key` is missing', function(done) {
+      var fn = function() {CloudfrontUtil.getSignedRTMPUrl('foo.com');};
+      expect(fn).to.throw(Error, /s3 key doesn't look right/i);
+      done();
+    });
+
+    it('should fail if `s3key` is invalid', function(done) {
+      var fn = function() {CloudfrontUtil.getSignedRTMPUrl('foo.com', '/key');};
+      expect(fn).to.throw(Error, /s3 key doesn't look right/i);
+      done();
+    });
+  });
+
+  describe('#normalizeSignature()', function() {
+    it('should translate illegal characters', function(done) {
+      var illegalChars = ['+', '=', '/'];
+      var arg = illegalChars.join('');
+      var sig = CloudfrontUtil.normalizeSignature(arg);
+
+      expect(sig).to.equal('-_~');
+      done();
+    });
+
+    it('should not alter valid characters', function(done) {
+      var sig = CloudfrontUtil.normalizeSignature('test+str');
+
+      expect(sig).to.equal('test-str');
+      done();
+    });
+  });
 });
-
-/**
- * Parse a URL string
- *
- * @param {String} Full url to parse
- * @return {Object}
- */
-function parseUrl(str) {
-  var _url = url.parse(str);
-  _url.query = querystring.parse(_url.query);
-
-  return _url;
-}
